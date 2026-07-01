e6dd4b67649558a28483fe5e81b85ae6515612a5
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-07-01 21:11:51 +0300

    implement home screen layout and add app color constants

---
+++ lib/core/theming/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color darkBackground = Color(0xFF0B1326);
+
+  static const Color indigoAccent = Color(0xFF818CF8);
+
+  static const Color iceBlue = Color(0xFFDAE2FD);
+
+}
